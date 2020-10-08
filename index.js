aa1c3a8e90af5fe40ccf473182c62b1336fa08b7
brijprivate <brijbcet@gmail.com>
2020-10-08 13:41:34 +0530

    a

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -172,4 +171,3 @@ app.listen(port,() => {
     console.log(`Server running at port `+port);
   });
 
-  exports.app = functions.https.onRequest(app);
